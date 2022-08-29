d81c588cf11376e6bb397e0c38a508665e37c15d
Tincho44 <martinharbuet@gmail.com>
2022-08-29 20:30:00 -0300

    V1.1

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -14,6 +14,8 @@ namespace SRP
         // Al igual que el metodo ShelveBook, el string LibrarySector y el string
         // LibraryShelve deben ir a otra clase para cumplir con SRP, dado que
         // no le corresponde tener esta responsabilidad a la clase Book
+        // Lo que haria seria hacer que ShelveBook sea una Clase y moveria estos
+        // 2 strings con la clase ShelveBook
 
         public Book(String title, String author, String code)
         {
@@ -25,6 +27,7 @@ namespace SRP
         // La clase libro deberia de tener menos responsabilidad, incumple con SRP
         // SRP tiene que tener 1 responsabilidad o una que se complemente por clase
         // Este metodo deberia de estar en otra clase
+
         public void ShelveBook(String sector, String shelve)
         {
             this.LibrarySector = sector;
